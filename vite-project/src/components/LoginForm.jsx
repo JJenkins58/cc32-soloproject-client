ac4311bae004ca8d99f43305652ca23d2e678bcd
JJenkins58 <knights70@gmail.com>
2024-07-12 06:29:39 +0900

    added id to login header

--- vite-project/src/components/LoginForm.jsx
+++ vite-project/src/components/LoginForm.jsx
@@ -53,7 +53,7 @@ export default function LoginForm() {
         {!correctStatusCode ?
         <div id='loginform_login-container' className='login-container'>
             <h1 id='login_title' className='title'>StyleSync</h1>
-            <h2 className='loginHeader'>Login Form</h2>
+            <h2 id='login_login-header' className='loginHeader'>Login Form</h2>
             <form onSubmit={handleSubmit(onSubmit)} noValidate>
                 <div className='form-control'>
                     <label htmlFor='username'>Username</label>
