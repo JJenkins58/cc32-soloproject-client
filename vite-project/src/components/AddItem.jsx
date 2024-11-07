5a9bd78b418f530c21c07004726c2ec40d4f624c
JJenkins58 <knights70@gmail.com>
2024-11-08 06:36:14 +0900

    added id to add-item_header

--- vite-project/src/components/AddItem.jsx
+++ vite-project/src/components/AddItem.jsx
@@ -46,7 +46,7 @@ export default function AddItem() {
     return (
         <div id='add-item_login-container' className='login-container'>
             <Avatar />
-            <h1 id='additem_header' className='additem-header'>AddItem</h1>
+            <h1 id='add-item_header' className='additem-header'>AddItem</h1>
             <form id='additem_additemForm' className='additemForm' onSubmit={handleSubmit(onSubmit)} noValidate>
                 
                 <div id='additem_form-control' className='form-control'>
