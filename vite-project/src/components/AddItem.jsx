52696497c39a80e3005e17c52e8d90e4c28ae758
JJenkins58 <knights70@gmail.com>
2024-12-07 06:14:12 +0900

    added id to add-item_button_add-item

--- vite-project/src/components/AddItem.jsx
+++ vite-project/src/components/AddItem.jsx
@@ -79,7 +79,7 @@ export default function AddItem() {
                     <p id='add-item_error_message_category' className='error'>{errors.category?.message}</p>
                 </div>
 
-                <button id='additem_button_add-item' className='link' type='submit'>Add Item</button>
+                <button id='add-item_button_add-item' className='link' type='submit'>Add Item</button>
             </form>
 
             <Link to='/home'>
